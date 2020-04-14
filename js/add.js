24964af745f3496b502c90407a771737811416ee
anasattili <anasattily@yahoo.com>
2020-04-14 23:34:58 +0300

    add Java script to web page

---
+++ js/add.js
@@ -0,0 +1,28 @@
+ 
+
+ alert('Welcome to Jordan');
+ 
+ confirm('do you love jordan?');
+
+ 
+
+
+var x ;
+
+ x= prompt('enter a number greater than 0');
+
+var greeting;
+
+
+if (x > 0 && x <= 10) {
+ greeting = 'Good';
+ } else if (x > 10 && x<20) {
+ greeting = 'Good morning';
+ } else if (x >= 20) {
+ greeting = 'Good night';
+ } else {
+ greeting = 'Good byu';
+ }
+document.write('<h1>'+greeting+'</h1>')
+
+
